e4a65cefbdd7d77f100eaa4ebff11513f01d8668
Simran Bhurat <44201011+simranbhurat@users.noreply.github.com>
2022-06-03 00:08:07 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -847,7 +847,7 @@ if (check == 'Hofstede'):
     corr = merged_df.corr()
     st.write(corr.style.background_gradient(cmap='Pastel1', axis=None, vmin=-1, vmax=1).highlight_null(null_color='#f1f1f1').set_precision(2))
     st.write("H -> Human List")
-    st.writw("R -> Random List")
+    st.write("R -> Random List")
 
 if(check == 'PCA'):
     pre_trained = st.sidebar.selectbox("Select a pre-trained model", ('Wikipedia','Google News', 'Reddit', 'Twitter' ))
